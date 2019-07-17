0954c24f4920d864293553d0716ec2f33624381b
Jason <jlu787@outlook.com>
2019-07-17 15:15:48 +1200

    Added in GreyBoxes
    Started working on cops

---
+++ SouthsideRun/Assets/Scripts/Cops.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cops : MonoBehaviour
+{
+    // PUBLIC
+    public float movespeed = 3.3333f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + movespeed * Time.deltaTime);
+    }
+}
