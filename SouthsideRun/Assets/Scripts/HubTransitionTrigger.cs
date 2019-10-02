4002d0d1b2cce7a271ae551d98ba5c083f102e57
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2019-10-02 13:05:20 +1300

    HUB transition/teleport trigger

---
+++ SouthsideRun/Assets/Scripts/HubTransitionTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubTransitionTrigger : MonoBehaviour
+{
+    public int toLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            switch(toLevel)
+            {
+                case 1:
+                    SceneManager.LoadScene("Level1Test");
+                    break;
+                case 2:
+                    //SceneManager.LoadScene("Level2");
+                    break;
+                case 3:
+                    //SceneManager.LoadScene("Level3");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
