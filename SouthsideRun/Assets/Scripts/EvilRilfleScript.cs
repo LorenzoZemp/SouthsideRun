8d1e455f0e281b92d46add57efb6b3567c7d59cc
Lorenzo Sae-Phoo Zemp <lorenzo.zem8065@mediadesign.school.nz>
2019-10-08 16:18:09 +1300

    working on god shooting you

---
+++ SouthsideRun/Assets/Scripts/EvilRilfleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilRilfleScript : MonoBehaviour
+{
+    public GameObject rifleBarrel;
+    public GameObject bulletPrefab;
+    public float timeToShoot = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToShoot -= Time.deltaTime;
+        if (timeToShoot - Time.deltaTime <= 0.0)
+        {
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        Instantiate(bulletPrefab, new Vector3(transform.position.x, transform.position.y + 1.2f, transform.position.z + 0.4f), Quaternion.Euler(90, 0, 0));
+    }
+}
---
+++ SouthsideRun/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(new Vector3(player.transform.position.x, player.transform.position.y + (float)1.3, player.transform.position.z));
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lineRenderer.SetPosition(1, new Vector3(0, 0, hit.distance));
+            }
+            else
+            {
+                lineRenderer.SetPosition(1, new Vector3(0, 0, 5000));
+            }
+        }
+    }
+}
