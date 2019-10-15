19337d98e29b38b5fc85ff5d5615e2364d251ff7
Lorenzo <lorenzo.zem8065@mediadesign.school.nz>
2019-10-16 10:29:43 +1300

    Reworked HUBTransition trigger

--- SouthsideRun/Assets/Scripts/HubTransitionTrigger.cs
+++ SouthsideRun/Assets/Scripts/HubTransitionTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class HubTransitionTrigger : MonoBehaviour
 {
-    public int toLevel;
+    public string toLevel;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +18,29 @@ public class HubTransitionTrigger : MonoBehaviour
         {
             switch(toLevel)
             {
-                case 1:
-                    SceneManager.LoadScene("Level1Test");
+                //Tutorial Level -> Hub1
+                case "HUB1":
+                    SceneManager.LoadScene("HUB Level 1");
                     break;
-                case 2:
-                    //SceneManager.LoadScene("Level2");
+                //Hub1 -> Level1;
+                case "LEVEL1":
+                    SceneManager.LoadScene("Level 1");
                     break;
-                case 3:
-                    //SceneManager.LoadScene("Level3");
+                //Level1 -> Hub2
+                case "HUB2":
+                    SceneManager.LoadScene("HUB Level 2");
+                    break;
+                //Hub -> Level2;
+                case "LEVEL2":
+                    SceneManager.LoadScene("Level 2");
+                    break;
+                //Level2 -> Hub3;
+                case "HUB3":
+                    SceneManager.LoadScene("HUB Level 3");
+                    break;
+                //Hub -> Level3;
+                case "LEVEL3":
+                    SceneManager.LoadScene("Level 3");
                     break;
                 default:
                     break;
